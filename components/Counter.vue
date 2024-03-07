5c0a802956e1cb545032f101016350375021b46b
Zan Fras <zan.fraas@gmail.com>
2024-03-07 16:45:15 +0100

    init

---
+++ components/Counter.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+const count = ref(0);
+</script>
+
+<template>
+  <div>
+    <button @click="count++">Increase</button>
+    <span>Count: {{ count }}</span>
+    <button @click="count--">Increase</button>
+  </div>
+</template>
